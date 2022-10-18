9d3dc2fb256969f7e87048bccc140094fac33749
chiaramery <meringolachiara@gmail.com>
2022-10-18 16:27:39 +0200

    Prova bomb square

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /* 
     [*] Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco.
-    [] Il computer deve generare 16 numeri casuali: le bombe.
-    [] I numeri nella lista delle bombe non possono essere duplicati.
+    [*] Il computer deve generare 16 numeri casuali: le bombe.
+    [*] I numeri nella lista delle bombe non possono essere duplicati.
     [] In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati, la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
     [] La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
     [] Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
@@ -19,13 +19,27 @@ headerBtn.innerHTML += `<button>Play</button>`;
 headerBtn.addEventListener("click", reload);
 
 /*                              MAIN                         */
+
+// BOMBE
+// Creo array di bombe (16 numeri casuali da 1 a 100)
+const arrayBombs = [];
+while(arrayBombs.length < 16) {
+    const rndNumberBombs = rndNumberBomb(1, 100);
+    if(!arrayBombs.includes(rndNumberBombs)) {
+        arrayBombs.push(rndNumberBombs);
+    }
+    console.log(rndNumberBombs);
+}
+console.log(arrayBombs);
+
+function redBomb () {
+    const bomb = document.getElementsByClassName("grid");
+    bomb.classList.add("red");
+}
 // Creo array di numeri da 1 a 100
 const numbersArray = [];
 const squareGrid = 100;
 const generetorNumber = isDuoblet(squareGrid);
-
-// Creo array di bombe (16 numeri casuali da 1 a 100)
-const arrayBombs = [];
 
 // Ordino i numeri in modo crescente
 for (let i = 0; i < numbersArray.length; i++) {
@@ -51,15 +65,16 @@ for (let i = 0; i < numbersArray.length; i++) {
 const grid = document.querySelector(".grid");
 for(let i = 0; i < generetorNumber.length; i++) {
     const thisNumber = generetorNumber[i];
+
     // Creo elemento 
     const thisSquare = createSquare(thisNumber);
+
     // Aggiungo addEventListener al click sull'elemento
     thisSquare.addEventListener("click", toColorSquare);
 
     // Elemento aggiunto nel DOM
     grid.append(thisSquare);
 }
-
 
 /*                            FUNZIONI                      */
 
@@ -109,19 +124,26 @@ function toColorSquare () {
     this.classList.add("blue");
     console.log("il valore di questa cella è:", clickSquare);
 }
-
-
+//PROVA SQUARE BOMB
+if(clickSquare.includes(arrayBombs.textContent)) {
+    squareGrid.classList.add("red");
+}
 /**
  * Description funzione che al click su bottone header ricarica la griglia
  * @returns {reload} reload della griglia di gioco
  */
 function reload () {
-    headerBtn = location.reload();
-    
+    headerBtn = location.reload();  
 }
-
-
-
+/**
+ * Description funzione che genera i numeri da 1 a 100 
+ * @param {number} min
+ * @param {number} max
+ * @returns {number} numero generato
+ */
+ function rndNumberBomb (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 /*                             FOOTER                        */
 const footerPara = document.querySelector(".made-by");
